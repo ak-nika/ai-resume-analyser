2d2a27fe6f12a57f6d0d7a2a405d9f94b5f26055
ak-nika <akingbayiojo@gmail.com>
2025-08-14 12:58:14 +0100

    Resume Feedback Page

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -84,7 +84,7 @@ const upload = () => {
     await kv.set(`resume:${uuid}`, JSON.stringify(data));
 
     setStatusText("Analysis complete, redirecting...");
-    console.log(data);
+    navigate(`/resume/${uuid}`);
   };
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
