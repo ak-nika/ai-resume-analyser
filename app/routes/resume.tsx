0c98d0a55ab48ef7b8d4feafac2f3f7839e4af32
ak-nika <akingbayiojo@gmail.com>
2025-08-14 14:08:12 +0100

    Resume Feedback Components

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router";
 import ATS from "~/components/ATS";
+import Details from "~/components/Details";
 import Summary from "~/components/Summary";
 import { usePuterStore } from "~/lib/puter";
 
@@ -26,7 +27,6 @@ const resume = () => {
 
   useEffect(() => {
     const loadResume = async () => {
-      console.log("loading resume...");
       const resume = await kv.get(`resume:${id}`);
 
       if (!resume) return console.log("no resume");
@@ -88,7 +88,12 @@ const resume = () => {
           <h2 className="text-4xl text-black font-bold">Resume Review</h2>
           {feedback ? (
             <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
-              <Summary feedback={feedback} /> <ATS score={feedback.ATS.score || 0} suggestions={feedback.ATS.tips || []}> <Details feedback={feedback} />
+              <Summary feedback={feedback} />
+              <ATS
+                score={feedback.ATS.score || 0}
+                suggestions={feedback.ATS.tips || []}
+              />
+              <Details feedback={feedback} />
             </div>
           ) : (
             <img
